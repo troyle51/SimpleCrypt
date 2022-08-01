3b77018068adb136d7273917beb888235db9ef7e
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-08-01 19:56:40 -0400

    pass tests

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -8,25 +8,35 @@ public class ROT13  {
     }
 
     ROT13() {
+        this('a', 'm');
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < text.length(); i++){
+            char c = text.charAt(i);
+            if       (c >= 'a' && c <= 'm') c += 13;
+            else if  (c >= 'A' && c <= 'M') c += 13;
+            else if  (c >= 'n' && c <= 'z') c -= 13;
+            else if  (c >= 'N' && c <= 'Z') c -= 13;
+            sb.append(c);
+        }
 
-        return "";
+        return sb.toString();
     }
 
     public String encrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
-
-        return "";
+        int index = s.indexOf(c);
+        return s.substring(index) + s.substring(0, index);
     }
 
 }
